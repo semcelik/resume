fd80b98968e8c960419630b45097313207e18f9e
Semih Çelik <s.celikce@gmail.com>
2020-08-20 21:19:24 +0300

    R-3: Add theme and design system

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React, { ReactElement } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+
+import useTheme from 'hooks/useTheme';
+import GlobalAppStyle from './App.style';
 
 function App(): ReactElement {
+  const { theme } = useTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalAppStyle />
+      <div style={{ backgroundColor: 'red' }}>test</div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes/lightThemePrimitives.ts
@@ -0,0 +1,11 @@
+import { TThemePrimitives } from 'types/theme';
+
+import color from './color';
+
+const lightThemePrimitives: TThemePrimitives = {
+  primary: color.red500,
+  primaryBackground: color.gray500,
+  primaryText: color.black900,
+};
+
+export default lightThemePrimitives;
