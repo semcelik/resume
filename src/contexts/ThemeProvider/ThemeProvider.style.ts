be51b03ebf7543ef53695b82f8763cc737537314
Semih Celik <s.celikce@gmail.com>
2021-01-11 16:03:42 +0300

    R-32: Change font to Roboto (#31)

--- src/contexts/ThemeProvider/ThemeProvider.style.ts
+++ src/contexts/ThemeProvider/ThemeProvider.style.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalThemeStyle = createGlobalStyle`
   html {
     font-size: 14px;
-    font-family: 'Lato', -apple-system, sans-serif;
+    font-family: 'Roboto', -apple-system, sans-serif;
     -webkit-font-smoothing: antialiased;
     -webkit-overflow-scrolling: touch;
     -moz-osx-font-smoothing: grayscale;
